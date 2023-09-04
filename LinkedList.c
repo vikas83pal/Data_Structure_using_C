365871ea2538df6f64029b030c69f1ab2e8815be
vikas pal <110290006+vikas83pal@users.noreply.github.com>
2023-09-04 15:14:19 +0530

    Update LinkedList.c

--- LinkedList.c
+++ LinkedList.c
@@ -7,8 +7,19 @@ struct node{
     int data;
     struct node* link;
 };
+
 struct node* head=NULL;
 int len;
+
+/*Creation Of a list*/
+void create(){
+    struct node* temp=(struct node*)malloc(sizeof(struct node));
+        printf("Enter data:");
+        scanf("%d",&temp->data);
+        //temp->link=NULL;
+        temp->link=head;
+        head=temp;
+}
 
 /*Function to insert a node at begin*/
 void insertAtBegin(){
@@ -45,6 +56,33 @@ void insertAtEnd(){
     }
 }
 
+/*Function to insert a node at middle*/
+void insertAtMiddle(){
+    struct node* temp,*p;
+    int loc,i,len;
+    printf("Enter loction To be inserted:");
+    scanf("%d",&loc);
+    len=length(head);
+    if(loc>len){
+        printf("Invalid loction.\n");
+        printf("Currently The List is Having %d Nodes",len);
+    }
+    else{
+        p=head;
+        while (i<loc)
+        {
+            p=p->link;
+            i++;
+        }        
+        temp=(struct node*)malloc(sizeof(struct node*));
+            printf("Enter data:");
+            scanf("%d",&temp->data);
+            temp->link=NULL;
+            temp->link=p->link;
+            p->link=temp;
+    }
+}
+
 /*Function to find length of the linked list*/
 int length(struct node* head) {
     struct node* temp = head;
@@ -75,52 +113,107 @@ void disp(){
 }
 
 /*function to display a node in reverse order*/
-void revdisp(){
-
+void revdisp() {
+    struct node* temp;
+    temp = head;
+    
+    if (temp == NULL) {
+        printf("LIST IS EMPTY\n");
+    } else {
+        struct node* prev = NULL;
+        
+        // Traverse the list while reversing the links
+        while (temp != NULL) {
+            struct node* next = temp->link;
+            temp->link = prev;
+            prev = temp;
+            temp = next;
+        }
+        
+        // Now 'prev' points to the new head of the reversed list
+        temp = prev;
+        
+        printf("Reversed List: ");
+        
+        while (temp != NULL) {
+            printf("%d->", temp->data);
+            temp = temp->link;
+        }
+        
+        printf("NULL\n");
+    }
 }
 
 /*function to del a node*/
 void del(){
-    
+    struct node* temp;
+    int loc;
+    printf("Enter the Loction Of The Node To Be Deleted:");
+    scanf("%d",&loc);
+    if(loc>length(head)){
+        printf("Invalid Loction\n");
+    }
+    else if(loc==1){
+        temp=head;
+        head=temp->link;
+        temp->link=NULL;
+        free(temp);
+        printf("Node Is Deleted\n");
+    }
+    else{
+        int i=1;
+        struct node* p=head,*q;
+        while(i<loc-1){
+            p=p->link;
+            i++;
+        }
+        q=p->link;
+        p->link=q->link;
+        q->link=NULL;
+        free(q);
+        printf("Node Is Deleted\n");
+    }
 }
 
 int main(){
     int ch;
     while(1){
-        printf("\nSINGLE LINKED LIST OPERATIONS\n");
-        printf("1. AddAtBegin\n");
-        printf("2. AddAtEnd\n");
-        printf("3. Display\n");
-        printf("4. length\n");
-        printf("5. Reverse Display\n");
-        printf("6. Exit\n");
+        printf("SINGLE LINKED LIST OPERATIONS\n");
+        printf("1. Creation\n");
+        printf("2. InsertAtBegin\n");
+        printf("3. InsertAtEnd\n");
+        printf("4. InsertAtMiddle\n");
+        printf("5. Display\n");
+        printf("6. length\n");
+        printf("7. Reverse Display\n");
+        printf("8. Deletion\n");
+        printf("9. Exit\n");
 
         printf("Enter The Choice:");
         scanf("%d",&ch);
         switch(ch){
-            case 1: insertAtBegin();
+            case 1: create();
                     break;
-            case 2: insertAtEnd();
+            case 2: insertAtBegin();
                     break;
-            case 3: disp();
+            case 3: insertAtEnd();
                     break;
-            case 4:
-                    len = length(head); // Call the length function with the head of your linked list
-                    printf("Length: %d\n", len); // Print the length
+            case 4: insertAtMiddle();
                     break;
-            case 5: revdisp();
+            case 5: disp();
                     break;
-            case 6: exit(1);
+            case 6: len = length(head); 
+                    printf("Length: %d\n", len); 
+                    break;
+            case 7: revdisp();
+                    break;
+            case 8: del();
+                    break;
+            case 9: exit(1);
                     break;
             default : printf("Invalid choice");
         }
     }
-
-
-
-
-
-
     getch();
     return 0;
 }
